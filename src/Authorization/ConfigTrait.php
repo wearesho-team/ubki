3877b5744e765aed7250689dea3ebc68e35e1f0a
KartaviK <varkuta.roma@gmail.com>
2018-07-06 19:52:34 +0300

    Implement Aothotization/Config for auth provider

---
+++ src/Authorization/ConfigTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Authorization;
+
+/**
+ * Trait ConfigTrait
+ *
+ * @package Wearesho\Bobra\Ubki\Authorization
+ */
+trait ConfigTrait
+{
+    /** @var string */
+    protected $username;
+
+    /** @var string */
+    protected $password;
+
+    /** @var string */
+    protected $mode;
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    public function getAuthUrl(): string
+    {
+        switch ($this->mode) {
+            case static::MODE_PRODUCTION:
+                return ConfigInterface::PRODUCTION_AUTH_URL;
+            case static::MODE_TEST:
+                return ConfigInterface::TEST_AUTH_URL;
+            default:
+                throw new \Exception("Mode have invalid value {$this->mode}");
+        }
+    }
+}
