4458588209163f0f57f21a97b106bdf0fc0ac1de
KartaviK <varkuta.roma@gmail.com>
2018-07-10 11:17:15 +0300

    fix tests for getting auth url from environment; removed unused property; fix tests for getting auth url from config

--- tests/Authorization/EnvironmentConfigTest.php
+++ tests/Authorization/EnvironmentConfigTest.php
@@ -5,7 +5,7 @@ namespace Wearesho\Bobra\Ubki\Tests\Authorization;
 use Gamez\Psr\Log\TestLogger;
 use Horat1us\Environment;
 use PHPUnit\Framework\TestCase;
-use Wearesho\Bobra\Ubki\Authorization;
+use Wearesho\Bobra\Ubki;
 
 /**
  * Class EnvironmentConfigTest
@@ -14,21 +14,19 @@ use Wearesho\Bobra\Ubki\Authorization;
  */
 class EnvironmentConfigTest extends TestCase
 {
-    /** @var Authorization\ConfigInterface */
+    /** @var Ubki\Authorization\ConfigInterface */
     protected $config;
 
     /** @var TestLogger */
     protected $logger;
 
-    protected $response;
-
     protected function setUp(): void
     {
         $this->logger = new TestLogger();
         $this->config =
-            new class('UBKI_') extends Environment\Config implements Authorization\ConfigInterface
+            new class('UBKI_') extends Environment\Config implements Ubki\Authorization\ConfigInterface
             {
-                use Authorization\EnvironmentConfigTrait;
+                use Ubki\Authorization\EnvironmentConfigTrait;
             };
 
         parent::setUp();
@@ -39,7 +37,6 @@ class EnvironmentConfigTest extends TestCase
         putenv('UBKI_USERNAME');
         $this->expectException(Environment\MissingEnvironmentException::class);
         $this->expectExceptionMessage('Missing environment key UBKI_USERNAME');
-
         $this->config->getUsername();
     }
 
@@ -47,7 +44,6 @@ class EnvironmentConfigTest extends TestCase
     {
         $username = 'username';
         putenv("UBKI_USERNAME={$username}");
-
         $this->assertEquals($username, $this->config->getUsername());
     }
 
@@ -63,7 +59,6 @@ class EnvironmentConfigTest extends TestCase
     {
         $password = 'password';
         putenv("UBKI_PASSWORD={$password}");
-
         $this->assertEquals($password, $this->config->getPassword());
     }
 
@@ -78,39 +73,46 @@ class EnvironmentConfigTest extends TestCase
     public function testGetProductionMode(): void
     {
         putenv('UBKI_PUSH_MODE=1');
-
         $this->assertEquals(true, $this->config->isProductionMode());
     }
 
     public function testGetTestMode(): void
     {
         putenv('UBKI_PUSH_MODE=0');
-
         $this->assertEquals(false, $this->config->isProductionMode());
     }
 
     public function testGetInvalidMode(): void
     {
         putenv('UBKI_PUSH_MODE=228');
-        $this->expectException(Authorization\UnsupportedModeException::class);
+        $this->expectException(Ubki\Authorization\UnsupportedModeException::class);
         $this->expectExceptionMessage('Mode have invalid value 228');
-
         $this->assertEquals(false, $this->config->isProductionMode());
     }
 
-    public function testGetTestEnvironmentAuthUrlFrom(): void
+    public function testGetTestDefaultAuthUrl(): void
     {
-        $mode = Authorization\ConfigInterface::MODE_TEST;
-        putenv("UBKI_PUSH_MODE={$mode}");
-        putenv(Authorization\ConfigInterface::TEST_AUTH_URL);
-        $this->assertEquals(Authorization\ConfigInterface::TEST_AUTH_URL, $this->config->getAuthUrl());
+        putenv('UBKI_PUSH_MODE=' . Ubki\Authorization\ConfigInterface::MODE_TEST);
+        $this->assertEquals(Ubki\Authorization\ConfigInterface::TEST_AUTH_URL, $this->config->getAuthUrl());
+    }
+
+    public function testGetProductionDefaulttAuthUrl(): void
+    {
+        putenv('UBKI_PUSH_MODE=' . Ubki\Authorization\ConfigInterface::MODE_PRODUCTION);
+        $this->assertEquals(Ubki\Authorization\ConfigInterface::PRODUCTION_AUTH_URL, $this->config->getAuthUrl());
+    }
+
+    public function testGetTestEnvironmentAuthUrl(): void
+    {
+        putenv('UBKI_AUTH_URL=' . Ubki\Authorization\ConfigInterface::TEST_AUTH_URL);
+        $url = getenv('UBKI_AUTH_URL');
+        $this->assertEquals(Ubki\Authorization\ConfigInterface::TEST_AUTH_URL, $url);
     }
 
     public function testGetProductionEnvironmentAuthUrl(): void
     {
-        $mode = Authorization\ConfigInterface::MODE_PRODUCTION;
-        putenv("UBKI_PUSH_MODE={$mode}");
-        putenv(Authorization\ConfigInterface::PRODUCTION_AUTH_URL);
-        $this->assertEquals(Authorization\ConfigInterface::PRODUCTION_AUTH_URL, $this->config->getAuthUrl());
+        putenv('UBKI_AUTH_URL=' . Ubki\Authorization\ConfigInterface::PRODUCTION_AUTH_URL);
+        $url = getenv('UBKI_AUTH_URL');
+        $this->assertEquals(Ubki\Authorization\ConfigInterface::PRODUCTION_AUTH_URL, $url);
     }
 }
