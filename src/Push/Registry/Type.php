3a26bfbc8b2d3afb0698e65a0b90085f31d4c349
KartaviK <varkuta.roma@gmail.com>
2018-07-11 16:48:44 +0300

    COmments for Registry types

--- src/Push/Registry/Type.php
+++ src/Push/Registry/Type.php
@@ -9,6 +9,13 @@ namespace Wearesho\Bobra\Ubki\Push\Registry;
  */
 interface Type
 {
+    /**
+     * Billing (getting the values of counters in the "Total", "New", "Corrected", "Errors", "Global duplicates",
+     * "Local duplicates", "Deleted")
+     */
     public const BIL = 'BIL';
+    /**
+     * Registry (receiving the registry of messages and errors).
+     */
     public const REP = 'REP';
 }
