33cc41bdfa194005ee73e574ea00346dc627fd6d
KartaviK <varkuta.roma@gmail.com>
2018-07-16 21:33:43 +0300

    add more types for elements

---
+++ src/Type/Document.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Type;
+
+use Wearesho\Bobra\Ubki;
+
+/**
+ * Class Document
+ * @package Wearesho\Bobra\Ubki\Type
+ */
+final class Document extends Ubki\Reference
+{
+    const PASSPORT = 1;
+    const DRIVER_LICENSE = 2;
+    const INTERNATIONAL_PASSPORT = 3;
+    const BIRTH_CERTIFICATE = 4;
+    const SEAMAN_PASSPORT = 5;
+    const MILITARY_TICKET = 6;
+    const TEMPORARY_IDENTITY_CARD = 7;
+    const DIPLOMA = 8;
+    const ATTESTAT = 9;
+    const PASSPORT_FOREIGN_CITIZEN = 10;
+    const CERTIFICATE_STATE_REGISTRATION = 11;
+    const EXTRACT_FROM_EGR = 12;
+    const ORDERING_FROM_EGR = 13;
+    const CERTIFICATE_REGISTRATION_TAXPAYERS = 14;
+    const INFORMATION_ABOUT_TAXPAYER_REGISTRATION = 15;
+    const PERMANENT_RESIDENCE_PERMIT = 16;
+    const PASSPORT_CITIZEN_UKRAINE_FORM_ID_CARD = 17;
+}
---
+++ src/Type/Nationality.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Type;
+
+use Wearesho\Bobra\Ubki;
+
+/**
+ * Class Nationality
+ * @package Wearesho\Bobra\Ubki\Type
+ *
+ * @method static static STATELESS(string $description = null)
+ * @method static static ARMENIA(string $description = null)
+ * @method static static AZERBAIJAN(string $description = null)
+ * @method static static BELARUS(string $description = null)
+ * @method static static GERMANY(string $description = null)
+ * @method static static GEORGIA(string $description = null)
+ * @method static static ISRAEL(string $description = null)
+ * @method static static KAZAKHSTAN(string $description = null)
+ * @method static static TURKEY(string $description = null)
+ * @method static static UKRAINE(string $description = null)
+ * @method static static USA(string $description = null)
+ * @method static static UZBEKISTAN(string $description = null)
+ * @method static static POLAND(string $description = null)
+ * @method static static RUSSIAN_FEDERATION(string $description = null)
+ */
+final class Nationality extends Ubki\Reference
+{
+    public const STATELESS = 1;
+    public const AZERBAIJAN = 31;
+    public const ARMENIA = 51;
+    public const BELARUS = 112;
+    public const GEORGIA = 268;
+    public const GERMANY = 276;
+    public const ISRAEL = 376;
+    public const KAZAKHSTAN = 398;
+    public const POLAND = 616;
+    public const RUSSIAN_FEDERATION = 643;
+    public const TURKEY = 792;
+    public const UKRAINE = 804;
+    public const USA = 840;
+    public const UZBEKISTAN = 860;
+}
