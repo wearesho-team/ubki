a8227784330388a0122641ae8e4ca1132c5e4c4d
KartaviK <varkuta.roma@gmail.com>
2018-07-18 19:05:39 +0300

    Fix name of variable

--- tests/Block/ContactTest.php
+++ tests/Block/ContactTest.php
@@ -14,21 +14,21 @@ use PHPUnit\Framework\TestCase;
 class ContactTest extends TestCase
 {
     /** @var Block\Contact */
-    protected $identifyingComponent;
+    protected $contacts;
 
     protected function setUp(): void
     {
         // todo: change string attribute to Contact Collection object
-        $this->identifyingComponent = new Block\Contact('Some contacts');
+        $this->contacts = new Block\Contact('Some contacts');
     }
 
     public function testType(): void
     {
-        $this->assertEquals(Block\Contact::ID, $this->identifyingComponent->getId());
+        $this->assertEquals(Block\Contact::ID, $this->contacts->getId());
     }
 
     public function testGetContacts(): void
     {
-        $this->assertEquals('Some contacts', $this->identifyingComponent->getContacts());
+        $this->assertEquals('Some contacts', $this->contacts->getContacts());
     }
 }
