e23db340b7b50d59b5f96eb6cb4a27c3e8a0837c
KartaviK <varkuta.roma@gmail.com>
2018-07-19 14:47:46 +0300

    Add Subject-role type

---
+++ tests/Type/SubjectRoleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Tests\Type;
+
+use Wearesho\Bobra\Ubki\Type\SubjectRole;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * class SubjectRoleTest
+ * @package Wearesho\Bobra\Ubki\Tests\Type
+ */
+class SubjectRoleTest extends TestCase
+{
+    public function testGuarantor(): void
+    {
+        $description = 'Поручитель';
+        $role = SubjectRole::GUARANTOR($description);
+        $this->assertEquals(SubjectRole::GUARANTOR, $role->getValue());
+        $this->assertEquals('GUARANTOR', $role->getKey());
+        $this->assertEquals($description, $role->getDescription());
+    }
+
+    public function testPledgor(): void
+    {
+        $description = 'Залогодатель';
+        $role = SubjectRole::PLEDGOR($description);
+        $this->assertEquals(SubjectRole::PLEDGOR, $role->getValue());
+        $this->assertEquals('PLEDGOR', $role->getKey());
+        $this->assertEquals($description, $role->getDescription());
+    }
+
+    public function testBorrower(): void
+    {
+        $description = 'Заемщик';
+        $role = SubjectRole::BORROWER($description);
+        $this->assertEquals(SubjectRole::BORROWER, $role->getValue());
+        $this->assertEquals('BORROWER', $role->getKey());
+        $this->assertEquals($description, $role->getDescription());
+    }
+}
