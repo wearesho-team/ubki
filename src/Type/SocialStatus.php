8dfbd4b6aaccdb47e04eaebacfb89cb9ed005601
KartaviK <varkuta.roma@gmail.com>
2018-07-19 13:49:27 +0300

    Refern methods

--- src/Type/SocialStatus.php
+++ src/Type/SocialStatus.php
@@ -7,6 +7,16 @@ use Wearesho\Bobra\Ubki;
 /**
  * Class SocialStatus
  * @package Wearesho\Bobra\Ubki\Type
+ *
+ * @method static static FULL_TIME(string $description = null)
+ * @method static static TERM_TIME(string $description = null)
+ * @method static static TEMP_UNEMPLOYED(string $description = null)
+ * @method static static PART_TIME(string $description = null)
+ * @method static static STUDENT(string $description = null)
+ * @method static static PENSIONER(string $description = null)
+ * @method static static PENSIONER_WORK(string $description = null)
+ * @method static static MATERNITY_LEAVE(string $description = null)
+ * @method static static OTHER(string $description = null)
  */
 final class SocialStatus extends Ubki\Reference
 {
@@ -19,49 +29,4 @@ final class SocialStatus extends Ubki\Reference
     const PENSIONER_WORK = 7;
     const MATERNITY_LEAVE = 8;
     const OTHER = 9;
-
-    public static function FULL_TIME(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::FULL_TIME, $description);
-    }
-
-    public static function TERM_TIME(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::TERM_TIME, $description);
-    }
-
-    public static function TEMP_UNEMPLOYED(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::TEMP_UNEMPLOYED, $description);
-    }
-
-    public static function PART_TIME(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::PART_TIME, $description);
-    }
-
-    public static function STUDENT(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::STUDENT, $description);
-    }
-
-    public static function PENSIONER(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::PENSIONER, $description);
-    }
-
-    public static function PENSIONER_WORK(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::PENSIONER_WORK, $description);
-    }
-
-    public static function MATERNITY_LEAVE(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::MATERNITY_LEAVE, $description);
-    }
-
-    public static function OTHER(string $description = null): SocialStatus
-    {
-        return new SocialStatus(static::OTHER, $description);
-    }
 }
