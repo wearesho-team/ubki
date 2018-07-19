a0a384ab333abef81d78ef9827b3d4505cb17397
KartaviK <varkuta.roma@gmail.com>
2018-07-19 14:10:39 +0300

    Add comments for currensies

--- src/Type/Currency.php
+++ src/Type/Currency.php
@@ -6,6 +6,7 @@ use Wearesho\Bobra\Ubki\Reference;
 
 /**
  * Class Currency
+ * Used iso-4217 standard
  * @package Wearesho\Bobra\Ubki\Type
  *
  * @method static static BYN(string $description = null)
@@ -22,15 +23,36 @@ use Wearesho\Bobra\Ubki\Reference;
  */
 class Currency extends Reference
 {
+    /** @var int The Belarusian Ruble */
     public const BYN = 974;
+
+    /** @var int Swiss frank */
     public const CHF = 756;
+
+    /** @var int Euro */
     public const EUR = 978;
+
+    /** @var int British Pound Sterling */
     public const GBP = 826;
+
+    /** @var int Lari */
     public const GEL = 981;
+
+    /** @var int Polish Zloty */
     public const PLN = 985;
+
+    /** @var int Russian ruble */
     public const RUB = 643;
+
+    /** @var int Ukrainian Hryvnia */
     public const UAH = 980;
+
+    /** @var int U.S. dollar */
     public const USD = 840;
+
+    /** @var int Silver */
     public const XAG = 961;
+
+    /** @var int Gold */
     public const XAU = 959;
 }
