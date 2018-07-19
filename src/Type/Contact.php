ead4676469750e0d287e7fc9fce66d444551bdf6
Roman Varkuta <varkuta.roma@gmail.com>
2018-07-19 13:46:52 +0300

    revert methods

--- src/Type/Contact.php
+++ src/Type/Contact.php
@@ -15,29 +15,4 @@ final class Contact extends Ubki\Reference
     public const MOBILE = 3;
     public const EMAIL = 4;
     public const FAX = 5;
-
-    public static function HOME(string $description = null): Contact
-    {
-        return new Contact(static::HOME, $description);
-    }
-
-    public static function WORK(string $description = null): Contact
-    {
-        return new Contact(static::WORK, $description);
-    }
-
-    public static function MOBILE(string $description = null): Contact
-    {
-        return new Contact(static::MOBILE, $description);
-    }
-
-    public static function EMAIL(string $description = null): Contact
-    {
-        return new Contact(static::EMAIL, $description);
-    }
-
-    public static function FAX(string $description = null): Contact
-    {
-        return new Contact(static::FAX, $description);
-    }
 }
