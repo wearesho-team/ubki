c7cb6ce9312a2b5d38bfc5903433c342f75a660b
KartaviK <varkuta.roma@gmail.com>
2018-07-19 13:32:09 +0300

    Define gender-reference methods in class body

--- src/Type/Gender.php
+++ src/Type/Gender.php
@@ -7,12 +7,19 @@ use Wearesho\Bobra\Ubki;
 /**
  * Class Gender
  * @package Wearesho\Bobra\Ubki\Data
- *
- * @method static static MAN(string $description = null)
- * @method static static WOMAN(string $description = null)
  */
 final class Gender extends Ubki\Reference
 {
     public const MAN = 1;
     public const WOMAN = 2;
+
+    public static function MAN(string $description = null): Gender
+    {
+        return new Gender(static::MAN, $description);
+    }
+
+    public static function WOMAN(string $description = null): Gender
+    {
+        return new Gender(static::WOMAN, $description);
+    }
 }
