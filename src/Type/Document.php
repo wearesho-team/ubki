34765dfb354540063003ef8995f04b554efcb17d
KartaviK <varkuta.roma@gmail.com>
2018-07-19 13:37:20 +0300

    Define document-reference methods in class body

--- src/Type/Document.php
+++ src/Type/Document.php
@@ -7,24 +7,6 @@ use Wearesho\Bobra\Ubki;
 /**
  * Class Document
  * @package Wearesho\Bobra\Ubki\Type
- *
- * @method static static PASSPORT(string $description = null)
- * @method static static DRIVER(string $description = null)
- * @method static static INTERNATIONAL(string $description = null)
- * @method static static BIRTH(string $description = null)
- * @method static static SEAMAN(string $description = null)
- * @method static static MILITARY(string $description = null)
- * @method static static TEMP_CARD(string $description = null)
- * @method static static DIPLOMA(string $description = null)
- * @method static static ATTESTAT(string $description = null)
- * @method static static CITIZEN(string $description = null)
- * @method static static STATE_REGISTRATION(string $description = null)
- * @method static static EXTRACT_FROM_EGR(string $description = null)
- * @method static static ORDERING_FROM_EGR(string $description = null)
- * @method static static CERTIFICATE_TAXPAYERS(string $description = null)
- * @method static static INFORMATION_TAXPAYER(string $description = null)
- * @method static static RESIDENCE(string $description = null)
- * @method static static UKRAINE_CARD(string $description = null)
  */
 final class Document extends Ubki\Reference
 {
@@ -45,4 +27,89 @@ final class Document extends Ubki\Reference
     public const INFORMATION_TAXPAYER = 15;
     public const RESIDENCE = 16;
     public const UKRAINE_CARD = 17;
+
+    public static function PASSPORT(string $description = null): Document
+    {
+        return new Document(static::PASSPORT, $description);
+    }
+
+    public static function DRIVER(string $description = null): Document
+    {
+        return new Document(static::DRIVER, $description);
+    }
+
+    public static function INTERNATIONAL(string $description = null): Document
+    {
+        return new Document(static::INTERNATIONAL, $description);
+    }
+
+    public static function BIRTH(string $description = null): Document
+    {
+        return new Document(static::BIRTH, $description);
+    }
+
+    public static function SEAMAN(string $description = null): Document
+    {
+        return new Document(static::SEAMAN, $description);
+    }
+
+    public static function MILITARY(string $description = null): Document
+    {
+        return new Document(static::MILITARY, $description);
+    }
+
+    public static function TEMP_CARD(string $description = null): Document
+    {
+        return new Document(static::TEMP_CARD, $description);
+    }
+
+    public static function DIPLOMA(string $description = null): Document
+    {
+        return new Document(static::DIPLOMA, $description);
+    }
+
+    public static function ATTESTAT(string $description = null): Document
+    {
+        return new Document(static::ATTESTAT, $description);
+    }
+
+    public static function CITIZEN(string $description = null): Document
+    {
+        return new Document(static::CITIZEN, $description);
+    }
+
+    public static function STATE_REGISTRATION(string $description = null): Document
+    {
+        return new Document(static::STATE_REGISTRATION, $description);
+    }
+
+    public static function EXTRACT_FROM_EGR(string $description = null): Document
+    {
+        return new Document(static::EXTRACT_FROM_EGR, $description);
+    }
+
+    public static function ORDERING_FROM_EGR(string $description = null): Document
+    {
+        return new Document(static::ORDERING_FROM_EGR, $description);
+    }
+
+    public static function CERTIFICATE_TAXPAYERS(string $description = null): Document
+    {
+        return new Document(static::CERTIFICATE_TAXPAYERS, $description);
+    }
+
+    public static function INFORMATION_TAXPAYER(string $description = null): Document
+    {
+        return new Document(static::INFORMATION_TAXPAYER, $description);
+    }
+
+    public static function RESIDENCE(string $description = null): Document
+    {
+        return new Document(static::RESIDENCE, $description);
+    }
+
+    public static function UKRAINE_CARD(string $description = null): Document
+    {
+        return new Document(static::UKRAINE_CARD, $description);
+    }
 }
