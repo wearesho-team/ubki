704dd1d14a43bee7a0c8827800a511e6f70cb278
KartaviK <varkuta.roma@gmail.com>
2018-07-24 16:26:46 +0300

    Add Court decision block

---
+++ tests/Block/CourtDecisionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Tests\Block;
+
+use Wearesho\Bobra\Ubki\Block;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class CourtDecisionTest
+ * @internal
+ * @package Wearesho\Bobra\Ubki\Tests\Block
+ */
+class CourtDecisionTest extends TestCase
+{
+    /** @var Block\CourtDecision */
+    protected $courtDecisionsComponent;
+
+    protected function setUp(): void
+    {
+        // todo: change string attribute to CourtDecision element
+        $this->courtDecisionsComponent = new Block\CourtDecision('Some decisions');
+    }
+
+    public function testGetDecisions(): void
+    {
+        $this->assertEquals('Some decisions', $this->courtDecisionsComponent->getDecisions());
+    }
+}
