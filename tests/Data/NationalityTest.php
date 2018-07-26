661f19a7d01018508c7bf42dab88f17ae73637b7
KartaviK <varkuta.roma@gmail.com>
2018-07-27 02:21:44 +0300

    Refactor namespace

--- tests/Data/NationalityTest.php
+++ tests/Data/NationalityTest.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace Wearesho\Bobra\Ubki\Tests\Type;
+namespace Wearesho\Bobra\Ubki\Tests\Data;
 
-use Wearesho\Bobra\Ubki\Type\Nationality;
+use Wearesho\Bobra\Ubki\Data\Nationality;
+
 use PHPUnit\Framework\TestCase;
 
 /**
  * Class NationalityTest
  * @internal
- * @package Wearesho\Bobra\Ubki\Tests\Type
+ * @package Wearesho\Bobra\Ubki\Tests\Data
  */
 class NationalityTest extends TestCase
 {
