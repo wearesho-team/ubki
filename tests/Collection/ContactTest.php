a10f516a5dee98017903d49a3d8ced46eb7ec9da
KartaviK <varkuta.roma@gmail.com>
2018-07-26 12:22:17 +0300

    Extend tests for contact collections

--- tests/Collection/ContactTest.php
+++ tests/Collection/ContactTest.php
@@ -2,19 +2,90 @@
 
 namespace Wearesho\Bobra\Ubki\Tests\Collection;
 
-use Wearesho\Bobra\Ubki\Collection\Contact;
-
-use PHPUnit\Framework\TestCase;
+use Carbon\Carbon;
+use Wearesho\Bobra\Ubki;
 
 /**
  * Class ContactTest
  * @internal
  * @package Wearesho\Bobra\Ubki\Tests\Collection
  */
-class ContactTest extends TestCase
+class ContactTest extends Ubki\Tests\Extend\CollectionTestCase
 {
-    public function testType()
+    protected const TYPE = Ubki\Element\Contact::class;
+
+    /** @var Ubki\Collection\Contact collection */
+    protected $collection;
+
+    protected $fakePhoneNumbers;
+
+    protected $fakeDates;
+
+    protected $fakeInns;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
     {
-        $this->assertEquals(\Wearesho\Bobra\Ubki\Element\Contact::class, (new Contact())->type());
+        parent::__construct($name, $data, $dataName);
+    }
+
+    protected function setUp(): void
+    {
+        for ($i = 0; $i < rand(1, 20); $i++) {
+            $this->fakePhoneNumbers[] = [
+                // todo: implement Type\Contact::...
+                rand(1, 5),
+                '+' . rand(100000, 999999) . rand(100000, 999999)
+            ];
+
+            $this->fakeDates[] = Carbon::create(
+                rand(1985, 2020),
+                rand(1, 12),
+                rand(1, 30),
+                rand(1, 24),
+                rand(1, 60),
+                rand(1, 60)
+            );
+
+            $this->fakeInns[] = rand(0, 1) ? rand(10000, 99999) . rand(10000, 99999) : null;
+        }
+
+        $this->collection = new Ubki\Collection\Contact();
+
+        foreach ($this->fakePhoneNumbers as $index => $number) {
+            $this->collection->append(new Ubki\Element\Contact(
+                $this->fakeDates[$index],
+                $number[1],
+                $number[0],
+                $this->fakeInns[$index]
+            ));
+        }
+    }
+
+    public function testCount(): void
+    {
+        $this->assertEquals(count($this->fakePhoneNumbers), $this->collection->count());
+    }
+
+    public function testOffsetGet(): void
+    {
+        foreach ($this->fakePhoneNumbers as $index => $number) {
+            /** @var Ubki\Element\Contact $element */
+            $element = $this->collection->offsetGet($index);
+            $this->assertEquals($number[1], $element->getValue());
+            $this->assertEquals($this->fakeDates[$index], $element->getCreatedAt());
+            $this->assertEquals($this->fakeInns[$index], $element->getInn());
+        }
+    }
+
+    public function testAppendInvalid(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->collection->append(new class extends Ubki\Element
+        {
+            public function tag(): string
+            {
+                return 'test';
+            }
+        });
     }
 }
