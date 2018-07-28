0958f99f74fc7f3456c752575df2de49c76f0809
KartaviK <varkuta.roma@gmail.com>
2018-07-29 02:03:47 +0300

    Add Credit deal type

---
+++ src/Data/CreditDeal/Type.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Data\CreditDeal;
+
+use Wearesho\Bobra\Ubki\Reference;
+
+/**
+ * Class Type
+ * @package Wearesho\Bobra\Ubki\Data\CreditDeal
+ *
+ * @method static static REPLENISHMENT_WORKING_CAPITAL(string $description = null)
+ * @method static static ACQUISITION_FIXED_ASSETS(string $description = null)
+ * @method static static BUSINESS_DEVELOPMENT(string $description = null)
+ * @method static static MORTGAGE(string $description = null)
+ * @method static static CAR_PURCHASE(string $description = null)
+ * @method static static TRAINING(string $description = null)
+ * @method static static OTHER_CONSUMER_PURPOSES(string $description = null)
+ * @method static static COMMODITY_LOAN(string $description = null)
+ * @method static static COMMERCIAL_CREDIT(string $description = null)
+ * @method static static LOAN(string $description = null)
+ * @method static static INSURANCE(string $description = null)
+ * @method static static LEASING(string $description = null)
+ * @method static static SALE(string $description = null)
+ * @method static static FACTORING(string $description = null)
+ * @method static static BANK_GUARANTEE(string $description = null)
+ * @method static static BANK_ACCOUNT(string $description = null)
+ * @method static static STORAGE(string $description = null)
+ * @method static static RETAIL_SALE(string $description = null)
+ * @method static static SUPPLY_GOODS(string $description = null)
+ * @method static static SALE_OF_REAL_ESTATE(string $description = null)
+ * @method static static BARTER(string $description = null)
+ * @method static static GIFT(string $description = null)
+ * @method static static LEASE_RENTAL(string $description = null)
+ * @method static static RENTING_DWELLING(string $description = null)
+ * @method static static LENDING(string $description = null)
+ * @method static static INDEPENDENT(string $description = null)
+ * @method static static RENDERING_SERVICE(string $description = null)
+ * @method static static CARRIAGE(string $description = null)
+ * @method static static APPOINTMENT(string $description = null)
+ * @method static static COMMISSION(string $description = null)
+ * @method static static CREDIT_CARD(string $description = null)
+ * @method static static PLACEMENT_LOANS_DEPOSITS_BANKS(string $description = null)
+ * @method static static IMPORTED_LETTER_CREDIT_INTERNATIONAL(string $description = null)
+ * @method static static DOMESTIC_LETTER_CREDIT(string $description = null)
+ * @method static static GUARANTEE_INTERNATIONAL_OPENING(string $description = null)
+ * @method static static GUARANTEE_INTERNAL_OPENING(string $description = null)
+ * @method static static COUNTER_GUARANTEE_INTERNAL(string $description = null)
+ * @method static static STANDBY_LETTER_CREDIT_INTERNATIONAL_OPENING(string $description = null)
+ * @method static static GUARANTEE_INTERNAL_ADVISING(string $description = null)
+ * @method static static GUARANTEE_INTERNATIONAL_ADVISING(string $description = null)
+ * @method static static GUARANTEE_AVAL(string $description = null)
+ * @method static static CONSUMER_CREDIT(string $description = null)
+ * @method static static UNCERTAIN(string $description = null)
+ */
+final class Type extends Reference
+{
+    const REPLENISHMENT_WORKING_CAPITAL = 1;
+    const ACQUISITION_FIXED_ASSETS = 2;
+    const BUSINESS_DEVELOPMENT = 3;
+    const MORTGAGE = 4;
+    const CAR_PURCHASE = 5;
+    const TRAINING = 6;
+    const OTHER_CONSUMER_PURPOSES = 7;
+    const COMMODITY_LOAN = 8;
+    const COMMERCIAL_CREDIT = 9;
+    const LOAN = 10;
+    const INSURANCE = 11;
+    const LEASING = 12;
+    const SALE = 13;
+    const FACTORING = 14;
+    const BANK_GUARANTEE = 15;
+    const BANK_ACCOUNT = 16;
+    const STORAGE = 17;
+    const RETAIL_SALE = 18;
+    const SUPPLY_GOODS = 19;
+    const SALE_OF_REAL_ESTATE = 20;
+    const BARTER = 21;
+    const GIFT = 22;
+    const LEASE_RENTAL = 23;
+    const RENTING_DWELLING = 24;
+    const LENDING = 25;
+    const INDEPENDENT = 26;
+    const RENDERING_SERVICE = 27;
+    const CARRIAGE = 28;
+    const APPOINTMENT = 29;
+    const COMMISSION = 30;
+    const CREDIT_CARD = 31;
+    const PLACEMENT_LOANS_DEPOSITS_BANKS = 32;
+    const IMPORTED_LETTER_CREDIT_INTERNATIONAL = 33;
+    const DOMESTIC_LETTER_CREDIT = 34;
+    const GUARANTEE_INTERNATIONAL_OPENING = 35;
+    const GUARANTEE_INTERNAL_OPENING = 36;
+    const COUNTER_GUARANTEE_INTERNAL = 37;
+    const STANDBY_LETTER_CREDIT_INTERNATIONAL_OPENING = 38;
+    const GUARANTEE_INTERNAL_ADVISING = 39;
+    const GUARANTEE_INTERNATIONAL_ADVISING = 40;
+    const GUARANTEE_AVAL = 41;
+    const CONSUMER_CREDIT = 42;
+    const UNCERTAIN = 99;
+}
