aef6a4a7094d7e3a9cee2a443a1213879856be6d
KartaviK <varkuta.roma@gmail.com>
2018-07-29 03:56:17 +0300

    Start implementing parser for push response

--- src/Push/Parser.php
+++ src/Push/Parser.php
@@ -2,6 +2,9 @@
 
 namespace Wearesho\Bobra\Ubki\Push;
 
+use Carbon\Carbon;
+use Wearesho\Bobra\Ubki\Data;
+
 /**
  * Class Parser
  * @package Wearesho\Bobra\Ubki\Push
@@ -10,6 +13,21 @@ class Parser
 {
     public function parseResponse(string $xml)
     {
+        $document = simplexml_load_string($xml);
 
+        $stepsXml = $document
+            ->{Tag::EXPORT_ROOT}
+            ->trace
+            ->step;
+        $stepCollection = new Data\Step\Collection();
+
+        foreach ($stepsXml as $stepXml) {
+            $attributes = $stepXml->attributes();
+            $stepCollection->append(new Data\Step\Entity(
+                (string)$attributes['name'],
+                Carbon::parse($attributes['stm']),
+                Carbon::parse($attributes['ftm'])
+            ));
+        }
     }
 }
