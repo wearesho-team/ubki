a0a816850db405b9d06735a589113d41d0ed7656
KartaviK <varkuta.roma@gmail.com>
2018-07-29 04:36:02 +0300

    Add tests

--- tests/Block/CreditTest.php
+++ tests/Block/CreditTest.php
@@ -2,9 +2,10 @@
 
 namespace Wearesho\Bobra\Ubki\Tests\Block;
 
-use Wearesho\Bobra\Ubki\Block;
-
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
+
+use Wearesho\Bobra\Ubki;
 
 /**
  * Class CreditTest
@@ -13,18 +14,99 @@ use PHPUnit\Framework\TestCase;
  */
 class CreditTest extends TestCase
 {
-    /** @var Block\Credit */
+    /** @var Ubki\Block\Credit */
     protected $creditDeals;
 
     protected function setUp(): void
     {
-        // todo: change string attribute to CreditDeal object
-        $this->creditDeals = new Block\Credit('Some deals');
+        $this->creditDeals = new Ubki\Block\Credit(
+            new Ubki\Data\CreditDeal\Collection([
+                new Ubki\Data\CreditDeal\Entity(
+                    'identificator',
+                    Ubki\Data\Language::ENG(),
+                    'name',
+                    'last name',
+                    Carbon::parse('1998-03-12'),
+                    Ubki\Data\CreditDeal\Type::REPLENISHMENT_WORKING_CAPITAL(),
+                    Ubki\Data\CreditDeal\Collateral::BANK_METALS(),
+                    Ubki\Data\CreditDeal\RepaymentProcedure::PAYMENTS_INDIVIDUAL(),
+                    Ubki\Data\Currency::EUR(),
+                    2400.00,
+                    Ubki\Data\SubjectRole::BORROWER(),
+                    2400,
+                    new Ubki\Data\CreditDeal\DealLife\Collection([
+                        new Ubki\Data\CreditDeal\DealLife\Entity(
+                            'identificator',
+                            4,
+                            2018,
+                            Carbon::parse('2018-04-09'),
+                            Carbon::parse('2018-05-09'),
+                            Ubki\Data\CreditDeal\Status::OPEN(),
+                            10000.00,
+                            2400,
+                            2400,
+                            2400,
+                            20,
+                            1,
+                            1,
+                            1,
+                            Carbon::parse('2018-04-29'),
+                            Carbon::parse('2018-04-29')
+                        )
+                    ]),
+                    '1234567890',
+                    'middle name'
+                )
+            ])
+        );
     }
 
     public function testGetDeals()
     {
-        $this->assertEquals(Block\Credit::ID, $this->creditDeals->getId());
-        $this->assertEquals('Some deals', $this->creditDeals->getDeals());
+        $deals = new Ubki\Block\Credit(
+            new Ubki\Data\CreditDeal\Collection([
+                new Ubki\Data\CreditDeal\Entity(
+                    'identificator',
+                    Ubki\Data\Language::ENG(),
+                    'name',
+                    'last name',
+                    Carbon::parse('1998-03-12'),
+                    Ubki\Data\CreditDeal\Type::REPLENISHMENT_WORKING_CAPITAL(),
+                    Ubki\Data\CreditDeal\Collateral::BANK_METALS(),
+                    Ubki\Data\CreditDeal\RepaymentProcedure::PAYMENTS_INDIVIDUAL(),
+                    Ubki\Data\Currency::EUR(),
+                    2400.00,
+                    Ubki\Data\SubjectRole::BORROWER(),
+                    2400,
+                    new Ubki\Data\CreditDeal\DealLife\Collection([
+                        new Ubki\Data\CreditDeal\DealLife\Entity(
+                            'identificator',
+                            4,
+                            2018,
+                            Carbon::parse('2018-04-09'),
+                            Carbon::parse('2018-05-09'),
+                            Ubki\Data\CreditDeal\Status::OPEN(),
+                            10000.00,
+                            2400,
+                            2400,
+                            2400,
+                            20,
+                            1,
+                            1,
+                            1,
+                            Carbon::parse('2018-04-29'),
+                            Carbon::parse('2018-04-29')
+                        )
+                    ]),
+                    '1234567890',
+                    'middle name'
+                )
+            ])
+        );
+        $this->assertEquals(Ubki\Block\Credit::ID, $this->creditDeals->getId());
+        $this->assertEquals(
+            $deals->getDeals(),
+            $this->creditDeals->getDeals()
+        );
     }
 }
