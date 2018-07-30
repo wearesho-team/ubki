3bb8672be5187b2a24c028ead51bc959a8602d0b
KartaviK <varkuta.roma@gmail.com>
2018-07-30 12:12:01 +0300

    Refactor namespaces

--- tests/Data/RegistrationSpdTest.php
+++ tests/Data/RegistrationSpdTest.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace Wearesho\Bobra\Ubki\Tests\Type;
+namespace Wearesho\Bobra\Ubki\Tests\Data;
 
-use Wearesho\Bobra\Ubki\Type\RegistrationSpd;
 use PHPUnit\Framework\TestCase;
+
+use Wearesho\Bobra\Ubki\Data\RegistrationSpd;
 
 /**
  * Class RegistrationSpdTest
  * @internal
- * @package Wearesho\Bobra\Ubki\Tests\Type
+ * @package Wearesho\Bobra\Ubki\Tests\Data
  */
 class RegistrationSpdTest extends TestCase
 {
