b87cb70104e84937f160c2505a52b4352aaab30a
KartaviK <varkuta.roma@gmail.com>
2018-08-13 12:21:09 +0300

    fixed attributes names

--- src/Data/CourtDecision/Entity.php
+++ src/Data/CourtDecision/Entity.php
@@ -32,15 +32,17 @@ class Entity extends Element implements \JsonSerializable
     public const INN = 'inn';
     public const DATE = 'votedate';
     public const SUBJECT_STATUS = 'voteusrst';
+    public const SUBJECT_STATUS_REF = 'voteusrstref';
     public const COURT_DEAL_TYPE = 'votetype';
+    public const COURT_DEAL_TYPE_REF = 'votetyperef';
     public const COURT_NAME = 'votesudname';
     public const LEGAL_FACT = 'voteurfact';
-    public const LEGAL_FACT_REFERENCE = 'voteurfactref';
+    public const LEGAL_FACT_REF = 'voteurfactref';
     public const CREATED_AT = 'vdate';
     public const DOCUMENT_TYPE = 'votedoctype';
-    public const DOCUMENT_TYPE_REFERENCE = 'votedoctyperef';
+    public const DOCUMENT_TYPE_REF = 'votedoctyperef';
     public const AREA = 'votesudregion';
-    public const AREA_REFERENCE = 'votesudregionref';
+    public const AREA_REF = 'votesudregionref';
     
     public function __construct(
         string $id,
