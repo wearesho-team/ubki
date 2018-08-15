c3a9a05356c7bca83739676d76c1da54dcdae836
KartaviK <varkuta.roma@gmail.com>
2018-08-15 15:10:31 +0300

    add production push url

--- src/Push/ConfigInterface.php
+++ src/Push/ConfigInterface.php
@@ -12,7 +12,7 @@ use Wearesho\Bobra\Ubki\Authorization;
 interface ConfigInterface extends Authorization\ConfigInterface
 {
     public const PRODUCTION_REGISTRY_URL = 'https://secure.ubki.ua/upload/in/reestrs.php';
-    public const PRODUCTION_PUSH_URL = ''; // todo: fill in release version
+    public const PRODUCTION_PUSH_URL = 'https://secure.ubki.ua/upload/data/xml';
 
     public const TEST_REGISTRY_URL = 'https://secure.ubki.ua:4040/upload/in/reestrs.php';
     public const TEST_PUSH_URL = 'https://secure.ubki.ua:4040/upload/data/xml';
