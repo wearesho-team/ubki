1cf5ada03e3904a91ab8dbf90082599e7b369ea8
KartaviK <varkuta.roma@gmail.com>
2018-08-31 15:43:44 +0300

    Add Compromised phone type

---
+++ src/References/CompromisedPhoneType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\References;
+
+use Wearesho\Bobra\Ubki\Reference;
+
+/**
+ * Class CompromisedPhoneType
+ * @package Wearesho\Bobra\Ubki\References
+ */
+final class CompromisedPhoneType extends Reference
+{
+    public const SCAMMER = 1;
+    public const PAYPHONE = 2;
+    public const FRICKER = 3;
+}
