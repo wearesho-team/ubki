38b115520614c24db728d6d1f270b269daf22c83
KartaviK <varkuta.roma@gmail.com>
2018-10-30 14:24:00 +0200

    Implemnt address type enum

---
+++ tests/Unit/Enum/AddressTypeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Tests\Unit\Enum;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\Bobra\Ubki\Enum\AddressType;
+
+/**
+ * Class AddressTypeTest
+ * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ */
+class AddressTypeTest extends TestCase
+{
+    public function testEnums(): void
+    {
+        $this->assertEquals(AddressType::REGISTRATION(), new AddressType(AddressType::REGISTRATION));
+        $this->assertEquals(AddressType::HOME(), new AddressType(AddressType::HOME));
+        $this->assertEquals(AddressType::ACTUAL(), new AddressType(AddressType::ACTUAL));
+        $this->assertEquals(AddressType::LEGAL(), new AddressType(AddressType::LEGAL));
+        $this->assertEquals(AddressType::MAILING(), new AddressType(AddressType::MAILING));
+        $this->assertEquals(AddressType::WORK(), new AddressType(AddressType::WORK));
+    }
+}
