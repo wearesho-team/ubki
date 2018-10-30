65cc8fc527a6ed143d3a535881b40db72ba11480
KartaviK <varkuta.roma@gmail.com>
2018-10-30 16:39:50 +0200

    Fix test

--- tests/Unit/Enum/RepaymentProcedureTest.php
+++ tests/Unit/Enum/RepaymentProcedureTest.php
@@ -19,55 +19,55 @@ class RepaymentProcedureTest extends TestCase
         );
         $this->assertEquals(
             RepaymentProcedure::OVERDRAFT(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::OVERDRAFT())
         );
         $this->assertEquals(
             RepaymentProcedure::PAYMENTS_INDIVIDUAL(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PAYMENTS_INDIVIDUAL())
         );
         $this->assertEquals(
             RepaymentProcedure::NON_REVOLVING_CREDIT_LINE(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::NON_REVOLVING_CREDIT_LINE())
         );
         $this->assertEquals(
             RepaymentProcedure::PAYMENT_MATURITY(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PAYMENT_MATURITY())
         );
         $this->assertEquals(
             RepaymentProcedure::PAYMENTS_MONTHLY(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PAYMENTS_MONTHLY())
         );
         $this->assertEquals(
             RepaymentProcedure::PERIODIC_FIVE_MONTH(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PERIODIC_FIVE_MONTH())
         );
         $this->assertEquals(
             RepaymentProcedure::PERIODIC_MONTH(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PERIODIC_MONTH())
         );
         $this->assertEquals(
             RepaymentProcedure::PERIODIC_QUARTERLY(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PERIODIC_QUARTERLY())
         );
         $this->assertEquals(
             RepaymentProcedure::PERIODIC_TWO_MONTH(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PERIODIC_TWO_MONTH())
         );
         $this->assertEquals(
             RepaymentProcedure::PERIODIC_TWO_WEEKS(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PERIODIC_TWO_WEEKS())
         );
         $this->assertEquals(
             RepaymentProcedure::PERIODIC_YEARLY(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::PERIODIC_YEARLY())
         );
         $this->assertEquals(
             RepaymentProcedure::REVOLVING_CREDIT_LINE(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::REVOLVING_CREDIT_LINE())
         );
         $this->assertEquals(
             RepaymentProcedure::SHEET_COMMITMENTS(),
-            new RepaymentProcedure(RepaymentProcedure::CREDIT_LIMIT())
+            new RepaymentProcedure(RepaymentProcedure::SHEET_COMMITMENTS())
         );
     }
 }
