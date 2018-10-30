f8988178189019f6290f78e6884b2916380c93c6
KartaviK <varkuta.roma@gmail.com>
2018-10-30 13:48:38 +0200

    Implement nationality enum

---
+++ src/Enum/Nationality.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Enum;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class Nationality
+ * @package Wearesho\Bobra\Ubki\Enum
+ *
+ * @method static Nationality STATELESS()
+ * @method static Nationality AZERBAIJAN()
+ * @method static Nationality ARMENIA()
+ * @method static Nationality BELARUS()
+ * @method static Nationality GEORGIA()
+ * @method static Nationality GERMANY()
+ * @method static Nationality ISRAEL()
+ * @method static Nationality KAZAKHSTAN()
+ * @method static Nationality POLAND()
+ * @method static Nationality RUSSIAN_FEDERATION()
+ * @method static Nationality TURKEY()
+ * @method static Nationality UKRAINE()
+ * @method static Nationality USA()
+ * @method static Nationality UZBEKISTAN()
+ */
+class Nationality extends Enum
+{
+    public const STATELESS = 1;
+    public const AZERBAIJAN = 31;
+    public const ARMENIA = 51;
+    public const BELARUS = 112;
+    public const GEORGIA = 268;
+    public const GERMANY = 276;
+    public const ISRAEL = 376;
+    public const KAZAKHSTAN = 398;
+    public const POLAND = 616;
+    public const RUSSIAN_FEDERATION = 643;
+    public const TURKEY = 792;
+    public const UKRAINE = 804;
+    public const USA = 840;
+    public const UZBEKISTAN = 860;
+}
---
+++ tests/Unit/Enum/NationalityTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Tests\Unit\Enum;
+
+use PHPUnit\Framework\TestCase;
+use Wearesho\Bobra\Ubki\Enum\Nationality;
+
+/**
+ * Class NationalityTest
+ * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ */
+class NationalityTest extends TestCase
+{
+    public function testEnums(): void
+    {
+        $this->assertEquals(Nationality::ARMENIA(), new Nationality(Nationality::ARMENIA));
+        $this->assertEquals(Nationality::BELARUS(), new Nationality(Nationality::BELARUS));
+        $this->assertEquals(Nationality::AZERBAIJAN(), new Nationality(Nationality::AZERBAIJAN));
+        $this->assertEquals(Nationality::GEORGIA(), new Nationality(Nationality::GEORGIA));
+        $this->assertEquals(Nationality::GERMANY(), new Nationality(Nationality::GERMANY));
+        $this->assertEquals(Nationality::ISRAEL(), new Nationality(Nationality::ISRAEL));
+        $this->assertEquals(Nationality::KAZAKHSTAN(), new Nationality(Nationality::KAZAKHSTAN));
+        $this->assertEquals(Nationality::POLAND(), new Nationality(Nationality::POLAND));
+        $this->assertEquals(Nationality::RUSSIAN_FEDERATION(), new Nationality(Nationality::RUSSIAN_FEDERATION));
+        $this->assertEquals(Nationality::STATELESS(), new Nationality(Nationality::STATELESS));
+        $this->assertEquals(Nationality::TURKEY(), new Nationality(Nationality::TURKEY));
+        $this->assertEquals(Nationality::UKRAINE(), new Nationality(Nationality::UKRAINE));
+        $this->assertEquals(Nationality::USA(), new Nationality(Nationality::USA));
+        $this->assertEquals(Nationality::UZBEKISTAN(), new Nationality(Nationality::UZBEKISTAN));
+    }
+}
