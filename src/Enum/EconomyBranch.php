1003d4a55efb63eda6c0740997c3969f942ce745
KartaviK <varkuta.roma@gmail.com>
2018-11-05 10:56:54 +0200

    Implement economy branch enum

---
+++ src/Enum/EconomyBranch.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Wearesho\Bobra\Ubki\Enum;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class EconomyBranch
+ * @package Wearesho\Bobra\Ubki\Enum
+ *
+ * @method static EconomyBranch AGRICULTURE()
+ * @method static EconomyBranch MINING_INDUSTRY()
+ * @method static EconomyBranch PROCESSING_INDUSTRY()
+ * @method static EconomyBranch SUPPLY_ELECTRICITY_GAS_AIR()
+ * @method static EconomyBranch WATER_SUPPLY_SEWAGE()
+ * @method static EconomyBranch BUILDING()
+ * @method static EconomyBranch WHOLESALE_AND_RETAIL_TRADE_IN()
+ * @method static EconomyBranch TRANSPORT_OR_STORAGE_FACILITIES_OR_COURIER_ACTIVITIES()
+ * @method static EconomyBranch TEMPORARY_ACCOMMODATION_OR_CATERING()
+ * @method static EconomyBranch INFORMATION_OR_TELECOMMUNICATIONS()
+ * @method static EconomyBranch FINANCIAL_OR_INSURANCE_ACTIVITIES()
+ * @method static EconomyBranch REAL_ESTATE_OPERATIONS()
+ * @method static EconomyBranch PROFESSIONAL_OR_SCIENTIFIC_OR_TECHNICAL_ACTIVITIES()
+ * @method static EconomyBranch ADMINISTRATIVE_OR_SUPPORT_SERVICES_1()
+ * @method static EconomyBranch ADMINISTRATIVE_OR_SUPPORT_SERVICES_2()
+ * @method static EconomyBranch EDUCATION()
+ * @method static EconomyBranch HEALTH_OR_SOCIAL_CARE()
+ * @method static EconomyBranch ARTS_OR_SPORTS_OR_ENTERTAINMENT_OR_RECREATION()
+ * @method static EconomyBranch PROVIDING_OTHER_TYPES_OF_SERVICES()
+ * @method static EconomyBranch HOUSEHOLD_ACTIVITIES()
+ * @method static EconomyBranch ACTIVITIES_OF_EXTRATERRITORIAL_ORGANIZATIONS_AND_BODIES()
+ */
+final class EconomyBranch extends Enum
+{
+    public const AGRICULTURE = 'A';
+    public const MINING_INDUSTRY = 'B';
+    public const PROCESSING_INDUSTRY = 'C';
+    public const SUPPLY_ELECTRICITY_GAS_AIR = 'D';
+    public const WATER_SUPPLY_SEWAGE = 'E';
+    public const BUILDING = 'F';
+    public const WHOLESALE_AND_RETAIL_TRADE_IN = 'G';
+    public const TRANSPORT_OR_STORAGE_FACILITIES_OR_COURIER_ACTIVITIES = 'H';
+    public const TEMPORARY_ACCOMMODATION_OR_CATERING = 'I';
+    public const INFORMATION_OR_TELECOMMUNICATIONS = 'J';
+    public const FINANCIAL_OR_INSURANCE_ACTIVITIES = 'K';
+    public const REAL_ESTATE_OPERATIONS = 'L';
+    public const PROFESSIONAL_OR_SCIENTIFIC_OR_TECHNICAL_ACTIVITIES = 'M';
+    public const ADMINISTRATIVE_OR_SUPPORT_SERVICES_1 = 'N';
+    public const ADMINISTRATIVE_OR_SUPPORT_SERVICES_2 = 'O';
+    public const EDUCATION = 'P';
+    public const HEALTH_OR_SOCIAL_CARE = 'Q';
+    public const ARTS_OR_SPORTS_OR_ENTERTAINMENT_OR_RECREATION = 'R';
+    public const PROVIDING_OTHER_TYPES_OF_SERVICES = 'S';
+    public const HOUSEHOLD_ACTIVITIES = 'T';
+    public const ACTIVITIES_OF_EXTRATERRITORIAL_ORGANIZATIONS_AND_BODIES = 'U';
+}
