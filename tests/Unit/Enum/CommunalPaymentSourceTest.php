d8eb804a9474db34eefdb1215984b33203ddb907
KartaviK <varkuta.roma@gmail.com>
2018-11-05 15:10:35 +0200

    Add miss phpdoc

--- tests/Unit/Enum/CommunalPaymentSourceTest.php
+++ tests/Unit/Enum/CommunalPaymentSourceTest.php
@@ -8,6 +8,8 @@ use Wearesho\Bobra\Ubki\Enum\CommunalPaymentSource;
 /**
  * Class CommunalPaymentSourceTest
  * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ * @coversDefaultClass \Wearesho\Bobra\Ubki\Enum\CommunalPaymentSource
+ * @internal
  */
 class CommunalPaymentSourceTest extends TestCase
 {
