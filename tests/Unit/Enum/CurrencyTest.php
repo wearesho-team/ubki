5a511234168ff6e03a67d060536317c67f6763b7
KartaviK <varkuta.roma@gmail.com>
2018-11-05 15:14:36 +0200

    Add miss phpdoc

--- tests/Unit/Enum/CurrencyTest.php
+++ tests/Unit/Enum/CurrencyTest.php
@@ -8,6 +8,8 @@ use Wearesho\Bobra\Ubki\Enum\Currency;
 /**
  * Class CurrencyTest
  * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ * @coversDefaultClass \Wearesho\Bobra\Ubki\Enum\Currency
+ * @internal
  */
 class CurrencyTest extends TestCase
 {
