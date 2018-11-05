cb6f5304266530ebf2643182c17fd460e32f7d18
KartaviK <varkuta.roma@gmail.com>
2018-11-05 15:25:42 +0200

    Add miss phpdoc

--- tests/Unit/Enum/DocumentTypeTest.php
+++ tests/Unit/Enum/DocumentTypeTest.php
@@ -8,6 +8,8 @@ use Wearesho\Bobra\Ubki\Enum\DocumentType;
 /**
  * Class DocumentTypeTest
  * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ * @coversDefaultClass \Wearesho\Bobra\Ubki\Enum\DocumentType
+ * @internal
  */
 class DocumentTypeTest extends TestCase
 {
