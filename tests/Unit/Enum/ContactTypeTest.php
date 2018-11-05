4f233bac3b4e89ba7d65c868e71d85700fcfd651
KartaviK <varkuta.roma@gmail.com>
2018-11-05 15:11:59 +0200

    Add miss phpdoc

--- tests/Unit/Enum/ContactTypeTest.php
+++ tests/Unit/Enum/ContactTypeTest.php
@@ -8,6 +8,8 @@ use Wearesho\Bobra\Ubki\Enum\ContactType;
 /**
  * Class ContactTypeTest
  * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ * @coversDefaultClass \Wearesho\Bobra\Ubki\Enum\ContactType
+ * @internal
  */
 class ContactTypeTest extends TestCase
 {
