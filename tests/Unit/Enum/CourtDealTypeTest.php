ec87e0f66102c06d121226d3fffaa7e59e01f9a1
KartaviK <varkuta.roma@gmail.com>
2018-11-05 15:13:12 +0200

    Add miss phpdoc

--- tests/Unit/Enum/CourtDealTypeTest.php
+++ tests/Unit/Enum/CourtDealTypeTest.php
@@ -8,6 +8,8 @@ use Wearesho\Bobra\Ubki\Enum\CourtDealType;
 /**
  * Class CourtDealTypeTest
  * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ * @coversDefaultClass \Wearesho\Bobra\Ubki\Enum\CourtDealType
+ * @internal
  */
 class CourtDealTypeTest extends TestCase
 {
