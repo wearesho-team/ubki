6378bdea85691ae8484a4556d616a5e8aa1250e0
KartaviK <varkuta.roma@gmail.com>
2018-11-05 15:39:08 +0200

    Add miss phpdoc

--- tests/Unit/Enum/NegativeFactorTypeTest.php
+++ tests/Unit/Enum/NegativeFactorTypeTest.php
@@ -8,6 +8,8 @@ use Wearesho\Bobra\Ubki\Enum\NegativeFactorType;
 /**
  * Class NegativeFactorTypeTest
  * @package Wearesho\Bobra\Ubki\Tests\Unit\Enum
+ * @coversDefaultClass \Wearesho\Bobra\Ubki\Enum\NegativeFactorType
+ * @internal
  */
 class NegativeFactorTypeTest extends TestCase
 {
